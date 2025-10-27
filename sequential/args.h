6da15603b6381967171bbdd961fff634d187a41e
andrey2605 <andrey26052001@gmail.com>
2025-10-27 19:42:07 +0000

    create openmp dir

---
+++ sequential/args.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Args {
+  int N{10};
+  int M{10};
+  int count_iter{-1};
+  double delta{0.01};
+  bool with_test{false};
+  double A1{-4.0};
+  double B1{4.0};
+  double A2{-1.0};
+  double B2{3.0};
+  double h1{0.0};
+  double h2{0.0};
+  double eps{0.0};
+  int precision{5};
+};
+
+Args parse_args(int argc, char *argv[]);
