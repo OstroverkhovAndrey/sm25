8ece7dedb4604512d8fb868753d34c8ea1f1e01b
andrey2605 <andrey26052001@gmail.com>
2025-10-29 21:52:48 +0000

    change graphics

--- graphics/boost.py
+++ graphics/boost.py
@@ -1,51 +1,30 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-# Фиксированный набор потоков на оси X
-threads = np.array([1, 2, 4, 8, 16, 32], dtype=int)
+# Данные
+d1 = {1: 1.000, 4: 1.680, 16: 1.059}
+d2 = {1: 1.000, 2: 2.008, 4: 3.974, 8: 7.280, 16: 8.583}
+d3 = {1: 1.000, 4: 3.999, 8: 7.522, 16: 8.623, 32: 9.185}
 
-cases = {
-    "40 X 40": {
-        "value": {1: 1.0, 2: 2.3, 4: 3.2,8: 4.1, 16: 4.3},
-        "color": "red",
-    },
-    "400 X 600": {
-        "value": {1: 1.0, 2: 2.3, 4: 3.2,8: 4.1, 16: 4.3, 32: 7.1},
-        "color": "green",
-    },
-    "800 X 1200": {
-        "value": {1: 1.0, 2: 2.3, 4: 3.2,8: 4.1, 16: 4.3, 32: 7.1},
-        "color": "blue",
-    },
-}
+# Фиксированные подписи по оси X (покажем все “классические” числа нитей)
+xticks = [1, 2, 4, 8, 16, 32]
 
-def speedup_array(time_by_threads, threads_axis):
-    # Вернёт массив ускорений согласно threads_axis, для отсутствующих нитей — NaN
-    y = []
-    for t in threads_axis:
-        if t in time_by_threads:
-            y.append(time_by_threads[t])
-        else:
-            y.append(np.nan)
-    return np.array(y, dtype=float)
-
-titles = list(cases.keys())
-
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 10), sharex=True, sharey=True)
-
-for ax, title in zip(axes, titles):
-
-    s = speedup_array(cases[title]['value'], threads)
-    ax.plot(threads, s, marker="o", color=cases[title]["color"])  # NaN создаёт разрывы линии
-
+def plot_runtime(ax, data: dict, title: str, color):
+    xs = sorted(data.keys())
+    ys = [data[t] for t in xs]
+    ax.plot(xs, ys, marker='o', linewidth=2, color=color)
     ax.set_title(title)
+    ax.set_ylabel('Ускорение')
     ax.grid(True, alpha=0.3)
-    ax.set_ylabel("Ускорение")
-    ax.set_xticks(threads)
-    ax.legend(loc="upper left")
+    ax.set_xticks(xticks)
 
-axes[-1].set_xlabel("Число нитей")
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 9), sharex=True)
 
-fig.suptitle("Зависимость ускорения от числа нитей", y=0.98)
+plot_runtime(axes[0], d1, '40 X 40', 'red')
+plot_runtime(axes[1], d2, '400 X 600', 'green')
+plot_runtime(axes[2], d3, '800 X 1200', 'blue')
+
+axes[-1].set_xlabel('Число нитей')
+
+fig.suptitle('Зависимость ускорения от числа нитей', y=0.98)
 fig.tight_layout()
 plt.show()
